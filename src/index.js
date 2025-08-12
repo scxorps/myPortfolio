d861819c1a2162f3dcd96e7c8144830768758fab
Mehadji Mohamed El Habib <mohamed79163@gmail.com>
2025-08-12 18:43:18 +0100

    Simplification complète: Portfolio standalone avec toutes les fonctionnalités

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import initTypewriter from "./scripts/typewriter";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
-initTypewriter();
